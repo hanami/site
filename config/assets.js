378f78b9e1393d55d43052b1f4b9ad8c5ee36fd7
Max Wheeler <max@makenosound.com>
2025-04-30 22:22:16 +1000

    Basic Tailwind v4 setup
    Works generally but fails to re-evaluate the bundle when `@source` dependencies change.

--- config/assets.js
+++ config/assets.js
@@ -1,16 +1,22 @@
 import * as assets from "hanami-assets";
+import postcss from "esbuild-postcss";
 
 await assets.run();
 
 // To provide additional esbuild (https://esbuild.github.io) options, use the following:
 //
 // Read more at: https://guides.hanamirb.org/assets/customization/
-//
-// await assets.run({
-//   esbuildOptionsFn: (args, esbuildOptions) => {
-//     // Add to esbuildOptions here. Use `args.watch` as a condition for different options for
-//     // compile vs watch.
-//
-//     return esbuildOptions;
-//   }
-// });
+await assets.run({
+  esbuildOptionsFn: (args, esbuildOptions) => {
+    // Add to esbuildOptions here. Use `args.watch` as a condition for different options for
+    // compile vs watch.
+
+    esbuildOptions.plugins = [
+      ...esbuildOptions.plugins,
+      // postcss is configured in postcss.config.js
+      postcss(),
+    ];
+
+    return esbuildOptions;
+  },
+});
