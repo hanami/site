07b8fe1c223cb714edce6dde792a0d81a2931b55
Tim Riley <tim@riley.id.au>
2025-08-16 12:04:37 +1000

    Enable scopes to have slots

---
+++ app/views/scope.rb
@@ -0,0 +1,15 @@
+# auto_register: false
+# frozen_string_literal: true
+
+module Site
+  module Views
+    class Scope < Hanami::View::Scope
+      def render_scope(scope_name, &block)
+        # `#scope` inside templates does not do anything with its given block at the moment. For the
+        # purposes of this gemo, save the block into a special local name that we then call inside
+        # the custom scope class' `#initialize`.
+        scope(scope_name, blk: block).render
+      end
+    end
+  end
+end
---
+++ app/views/scopes/card.rb
@@ -0,0 +1,30 @@
+# auto_register: false
+# frozen_string_literal: true
+
+module Site
+  module Views
+    module Scopes
+      class Card < Hanami::View::Scope
+        def initialize(**)
+          super
+          @slots = {}
+
+          # Evaluate the scope given to `#render_scope` so that we can fill our slots.
+          _locals[:blk]&.call(self)
+        end
+
+        def render(partial = "partials/card", **opts)
+          super
+        end
+
+        def slot(name, content = nil)
+          @slots[name] = (content || yield).html_safe
+        end
+
+        def render_slot(name)
+          @slots[name]
+        end
+      end
+    end
+  end
+end
