e5290a9f4607033caed34d738e9696b7f2632f1e
Josephine Hall <josephinehall@gmail.com>
2025-08-30 16:14:13 +1000

    Add repo

---
+++ app/repos/team_member_repo.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Site
+  module Repos
+    class TeamMemberRepo < Site::DB::Repo
+      def all
+        team_members.to_a
+      end
+
+      def active_members
+        # TODO
+      end
+    end
+  end
+end
