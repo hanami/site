e1a56aef789af50a379d13b216a98fad497580d2
Josephine Hall <josephinehall@gmail.com>
2025-08-31 22:02:01 +1000

    Parse markdown content into HTML

--- app/structs/team_member.rb
+++ app/structs/team_member.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require "html_pipeline"
+require "html_pipeline/convert_filter/markdown_filter"
+
 module Site
   module Structs
     class TeamMember < Site::DB::Struct
@@ -9,6 +12,25 @@ module Site
 
       def status
         active? ? "Active since #{active_since}" : "Former member"
+      end
+
+      def description_md
+        description
+      end
+
+      def description_html
+        @description_html ||= description_data.fetch(:output).html_safe
+      end
+
+      private
+
+      ContentPipeline = HTMLPipeline.new(
+        convert_filter: HTMLPipeline::ConvertFilter::MarkdownFilter.new
+      )
+      private_constant :ContentPipeline
+
+      def description_data
+        @description_data ||= ContentPipeline.call(description_md)
       end
     end
   end
